468a0c12b501ad15356bf42066ef479b69df2197
Joelson <joelsonsantos181040@gmail.com>
2026-09-05 21:09:53 -0300

    refactor(common): move config.fish para o módulo de configuração

--- common/fish/config.fish
+++ common/fish/config.fish
@@ -15,10 +15,28 @@ if type -q bat
     alias cat="bat --paging=never --style=plain"
 end
 
-# Inicialização do Starship Prompt
+# Atalho inteligente para ativar venvs python (digite apenas: activate)
+function activate
+    if test -f .venv/bin/activate.fish
+        builtin source .venv/bin/activate.fish
+    else if test -f venv/bin/activate.fish
+        builtin source venv/bin/activate.fish
+    else
+        echo "Nenhum ambiente virtual (.venv ou venv) encontrado no diretório atual."
+    end
+end
+
+# Se digitar 'source .venv/bin/activate' por força do hábito do bash, redireciona para o .fish
+function source
+    if string match -r '(.*)/bin/activate$' -- "$argv[1]" >/dev/null; and test -f "$argv[1].fish"
+        builtin source "$argv[1].fish"
+    else
+        builtin source $argv
+    end
+end
+
+# Configuração e inicialização do Starship Prompt
+set -gx STARSHIP_CONFIG "$HOME/.config/starship/starship.toml"
 if type -q starship
     starship init fish | source
 end
-
-# Configuração do Starship
-export STARSHIP_CONFIG="$HOME/.config/starship/starship.toml"
